3e6cce64e4fb97f3dc0e31459ba717713c7e234d
Elkana Muthengi <elkyem@gmail.com>
2023-09-27 01:50:46 +0300

    added printMinAndMax solution

--- InputCalculator/InputCalculator.java
+++ InputCalculator/InputCalculator.java
@@ -4,7 +4,33 @@ public class InputCalculator {
     
     public static void inputThenPrintSumAndAverage(){
         
-         
+		Scanner sc = new Scanner(System.in);
+		boolean test = true;
+		int input = 0;
+		double avg = 0L;
+		int sum = 0;
+		int i=0;
+		
+        while(test){
+			
+			try{
+				
+				input = Integer.parseInt(sc.nextLine());
+				
+				sum+=input;
+				System.out.println("cycle is "+i);
+				System.out.println("total is "+sum);
+				i++;
+				
+			}catch(NumberFormatException e){
+				System.out.println("cycle is "+i);
+				avg = (double)Math.round((sum/i));
+				System.out.println("SUM = "+sum+" AVG = "+avg);
+				test = false;
+				break;				
+			}
+			
+		}
         
     }
 	
