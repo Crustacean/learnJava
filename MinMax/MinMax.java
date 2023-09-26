4c4863c54a29b90f1ccd925d121c81b90505df43
Elkana Muthengi <elkyem@gmail.com>
2023-09-26 20:41:26 +0300

    added printMinAndMax solution

--- MinMax/MinMax.java
+++ MinMax/MinMax.java
@@ -5,38 +5,42 @@ public class MinMax{
 	public static void printMinAndMax(){
 		
 		Scanner sc = new Scanner(System.in);
+		boolean isNumber = true;
 		int myValue = 0;
 		int min = 0;
 		int max = 0;
+		int i = 1;
 		
 		do{
 			
-			System.out.print("Enter a number: ");
-			myValue = isNumber(sc.NextLine());
-			
-			if(max>myValue && min>myValue){
-				min = myValue;
-			}else{
-				max = myValue;
-			}		
-			
+			try{
+				
+				System.out.print("Enter a number: ");
+				myValue = Integer.parseInt(sc.nextLine());
+				
+				if(i==1){
+					min = myValue;
+					max = myValue;
+				}else{
+					if(myValue>max){
+					max = myValue;
+					}else if(myValue<max && min>myValue){
+						min = myValue;
+					}
+				}
+				i++;
+				
+			}catch(NumberFormatException e){
+				
+				System.out.println("Ending session.");
+				isNumber= false;				
+				break;
+				
+			}
 			
 		}while(isNumber);
 		
 		System.out.println("Min: "+min+", Max: "+max);
-		
-	}
-	
-	public static boolean isNumber(int num){
-		
-		try{
-			
-			Integer.parseInt(num);
-			return true;
-			
-		}catch(NumberFormatException e){
-			return false;
-		}		
 		
 	}
 	
