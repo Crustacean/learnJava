cb5094f6a5d2b2496feac30b7ba8d852a9aead5b
Elkana Muthengi <elkyem@gmail.com>
2023-09-27 02:55:04 +0300

    added PaintJob solution

--- PaintJob/PaintJob.java
+++ PaintJob/PaintJob.java
@@ -1,6 +1,6 @@
 public class PaintJob {
     
-    public static int getBucketCount(double width, double height, double areaPerBucket, double extraBuckets){
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
 				
 		if(width<=0 || height<=0 || areaPerBucket<=0 || extraBuckets<0){
 			return -1;
