97b8c7bf632b84f420af7f7f7024de9e594dac13
Elkana Muthengi <elkyem@gmail.com>
2023-09-27 00:52:42 +0300

    added InputCalculator solution

---
+++ InputCalculator/InputCalculator.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ 
+public class InputCalculator {
+    
+    public static void inputThenPrintSumAndAverage(){
+        
+         
+        
+    }
+	
+	public static void main(String[] args){
+		
+		inputThenPrintSumAndAverage();
+		
+	}
+    
+}
