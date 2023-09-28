9332c46b74d9137a900adedd98a76a003bbbd3cf
Elkana Muthengi <elkyem@gmail.com>
2023-09-28 21:16:13 +0300

    added BankAccount solution

---
+++ BankAccount/BankDetails.java
@@ -0,0 +1,22 @@
+public class BankDetails{
+	
+	private Long accountNumber;
+	private double accountBalance;
+	private String custName;
+	private string custEmail;
+	private Long custPhone;
+	
+	
+	public void deposit(){
+		
+		
+		
+	}
+	
+	public void withdraw(){
+		
+		
+		
+	}
+	
+}
---
+++ BankAccount/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+	
+	public static void main(String[] args){
+		
+		BankDetails bankDetails = new BankDetails();
+		
+		bankDetails.deposit();
+		bankDetails.withdraw();
+		
+		
+		
+	}
+	
+}
