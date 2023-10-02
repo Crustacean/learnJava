d0394d15f91cd76be84b38d2f6fa4956d082a482
Elkana Muthengi <elkyem@gmail.com>
2023-10-02 21:17:55 +0300

    added Customer solution

---
+++ Customer/Customer.java
@@ -0,0 +1,34 @@
+public Customer{
+	
+	private String name;
+	private int creditLimit;
+	private String email;
+	
+	public Customer(){
+		System.out.println("Initializing constructor...");
+	}
+	
+	public Customer(String name, int creditLimit, String email){
+		
+		this.name=name;
+		this.creditLimit=creditLimit;
+		this.email=email;
+		
+	}
+	
+	public String getName(){ return name;}
+	
+	public int getCreditLimit(){ return creditLimit; }
+	
+	public String email(){ return email; }
+	
+	public vod getCustomerDetails(){
+		
+		System.out.println("Name: "+name);
+		System.out.println("Credit Limit: "+creditLimit);
+		System.out.println("Email Address: "+email);
+		
+	}
+	
+	
+}
---
+++ Customer/Main.java
@@ -0,0 +1,9 @@
+public Mail{
+	
+	public static void main(String[] args){
+		
+		Customer aCustomer = new Customer(Bancy Nashipai, 2000, bancynashipai@gmail.com);
+		aCustomer.getCustomerDetails();
+	}
+	
+}
