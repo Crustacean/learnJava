cb6984059d5431ef72a93bfd80255ff4baa25849
Elkana Muthengi <elkyem@gmail.com>
2023-10-03 22:58:26 +0300

    added Student solution

---
+++ Student/Student.java
@@ -0,0 +1,17 @@
+public class Student{
+	
+	private String id;
+	private String name;
+	private String dateOfBirth;
+	private String classList;
+	
+	public Student(String id, String studentName, String dateOfBirth, String classList){
+		
+		this.id=id;
+		name=studentName;
+		this.dateOfBirth=dateOfBirth;
+		this.classList=classList;
+		
+	}
+	
+}
