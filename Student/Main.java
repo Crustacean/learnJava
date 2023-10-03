170816f967dfd92c67f7c9a4c9e3e7db7f8b6e8b
Elkana Muthengi <elkyem@gmail.com>
2023-10-03 23:21:44 +0300

    added Student solution

--- Student/Main.java
+++ Student/Main.java
@@ -1,8 +1,22 @@
+import java.util.*;
+
 public class Main{
 	
 	public static void main(String[] args){
 		
-		
+		for(int i=1; i<=5; i++){
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("Enter student's name: ");
+			String studentName = sc.nextLine();
+			System.out.print("Enter student's Date of Birth: ");
+			String studentDateOfBirth = sc.nextLine();
+			System.out.print("Enter student's class list: ");
+			String studentClassList=sc.nextLine();
+			
+			Student student = new Student(("IMTJO-"+i), studentName, studentDateOfBirth, studentClassList);
+			
+		}
 		
 	}
 	
