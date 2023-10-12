09d90601261b789682485086b65e6bfe2e911c00
Elkana Muthengi <elkyem@gmail.com>
2023-10-12 03:31:09 +0300

    added Complex Operations solution

--- Complex/ComplexNumber.java
+++ Complex/ComplexNumber.java
@@ -12,19 +12,23 @@ public class ComplexNumber {
     }
     
     public void add(double real, double imaginary){
-        
+        this.real += real;
+		this.imaginary += imaginary;
     }
     
     public add(ComplexNumber x){
-        
+        this.real = this.real+x.real;
+		this.imaginary = this.imaginary+.imaginary
     }
     
     public subtract(double real, double imaginary){
-        
+        this.real -+ real;
+		this.imaginary -= imaginary;
     }
     
-    public subtract(ComplexNumber x){
-        
+    public subtract(ComplexNumber y){
+        this.real = this.real-y.real;
+		this.imaginary = this.imaginary-y.imaginary;
     }
     
 }
