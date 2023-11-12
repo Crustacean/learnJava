b472faa1d1c30aaae343bc966eab484e06d8b69f
Elkana Muthengi <elkyem@gmail.com>
2023-11-12 15:28:33 +0300

    added Inheritance solution

--- Inheritance/Main.java
+++ Inheritance/Main.java
@@ -2,7 +2,16 @@ public class Main{
 	
 	public static void main(String[] args){
 		
+		Worker worker = new Worker("Anne","1990-07-23","2023-11-12");
 		
+		
+	}
+	
+	public static void getEmploymentDetails(Worker worker, ){
+		
+		worker.getAge();
+		worker.collectPay();
+		worker.terminate();
 		
 	}
 	
