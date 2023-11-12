1ed6936116fa1e199399e05029216181a8b8f565
Elkana Muthengi <elkyem@gmail.com>
2023-11-12 17:58:55 +0300

    added Inheritance solution

--- Inheritance/Main.java
+++ Inheritance/Main.java
@@ -2,11 +2,15 @@ public class Main{
 	
 	public static void main(String[] args){
 		
-		Employee bob = new Employee("Bob","1950-07-23","2019-01-07");
-		
+		Employee bob = new Employee("Bob","1950-07-23","2019-01-07");		
 		System.out.println(bob);
 		System.out.println("Age: " + bob.getAge());
 		System.out.println("Salary: " + bob.collectPay());
+		
+		Employee jane = new Employee("Jane","2000-03-10","2019-01-07");		
+		System.out.println(jane);
+		System.out.println("Age: " + jane.getAge());
+		System.out.println("Salary: " + jane.collectPay());
 		
 	}
 	
