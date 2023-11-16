f47c2a01f66259ea09f0a4988d338d1113603919
Elkana Muthengi <elkyem@gmail.com>
2023-11-16 20:16:21 +0300

    added Smart kitchen solution

---
+++ SmartKitchen/SmartKitchen.java
@@ -0,0 +1,84 @@
+public class SmartKitchen{
+	
+	private Coffeemaker brewMaster;
+	private DishWasher dishWasher;
+	private Refrigerator iceBox;
+	
+	public SmartKitchen(){
+		brewMaster = new CoffeeMaker();
+		dishWasher = new DishWasher();
+		iceBox = new Refrigerator();
+		
+	}
+	
+	public Coffeemaker getBrewMaster(){
+		return brewMaster;
+	}
+	
+	public DishWasher getDishWasher(){
+		return dishWasher;
+	}
+	
+	public Refrigerator getIceBox(){
+		return iceBox;
+	}
+	
+}
+
+class Refrigerator extends SmartKitchen{
+	
+	private boolean hasWorkToDo;
+	
+	public void setHasWorkToDo(boolean hasWorkToDo){
+		
+		this.hasWorkToDo = hasWorkToDo;
+		
+	}
+	
+	public void orderFood(){
+		
+		if(hasWorkToDo){
+			System.out.println("Cooling food.");
+			hasWorkToDo=false;
+		}
+	}
+	
+}
+
+class DishWasher extends SmartKitchen{
+	
+	private boolean hasWorkToDo;
+	
+	public void setHasWorkToDo(boolean hasWorkToDo){
+		
+		this.hasWorkToDo = hasWorkToDo;
+		
+	}
+	
+	public void doDishes(){
+		if(hasWorkToDo){
+			System.out.println("Doing dishes.");
+			hasWorkToDo=false;
+		}
+	}
+	
+}
+
+class CoffeeMaker extends SmartKitchen{
+	
+	private boolean hasWorkToDo;
+	
+	public void setHasWorkToDo(boolean hasWorkToDo){
+		
+		this.hasWorkToDo = hasWorkToDo;
+		
+	}
+	
+	public void brewCoffee(){
+		if(hasWorkToDo){
+			System.out.println("Brewing coffee.");
+			hasWorkToDo=false;
+		}
+	}
+	
+}
