3878dbba572428c459b4dcb0e13f2a60df3cdb74
Elkana Muthengi <elkyem@gmail.com>
2023-11-16 20:23:41 +0300

    added Smart kitchen solution

--- SmartKitchen/Main.java
+++ SmartKitchen/Main.java
@@ -4,8 +4,13 @@ public class Main{
 		
 		SmartKitchen smartKitchen = new SmartKitchen();
 		
+		smartKitchen.getBrewMaster().setHasWorkToDo(true);
 		smartKitchen.getBrewMaster().brewCoffee();
+		
+		smartKitchen.getDishWasher().setHasWorkToDo(true);
 		smartKitchen.getDishWasher().doDishes();
+		
+		smartKitchen.getIceBox().setHasWorkToDo(true);
 		smartKitchen.getIceBox().orderFood();
 		
 	}
