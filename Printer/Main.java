1a10c6aaf864e02a793f1d9337f5400fc1848823
Elkana Muthengi <elkyem@gmail.com>
2023-11-17 22:25:48 +0300

    added Printer solution

--- Printer/Main.java
+++ Printer/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 	
+	Printer printer = new Printer(30, 5, false);
 	
-	
+	System.out.println("Toner level: " + printer.addToner(20));
+	System.out.println("Pages to be printed: " + printPages(5));	
 	
 }
--- Printer/Printer.java
+++ Printer/Printer.java
@@ -4,15 +4,42 @@ public class Printer {
 	private int pagesPrinted;
 	private boolean duplex;
 	
+	public Printer(int tonerLevel, boolean duplex) {
+		
+		this(20, pagesPrinted, true);
+		
+	}
+	
+	public Printer(int tonerLevel, int pagesPrinted, boolean duplex) {
+		
+		this.tonerLevel = tonerLevel;
+		this.pagesPrinted = pagesPrinted;
+		this.duplex = duplex;
+		
+	}
+	
 	public int addToner(int tonerAmount) {
 		
+		tonerLevel += tonerAmount;
 		
+		if(tonerLevel > 100 || tonerLevel < 0){
+			return -1;
+		}
+		
+		return tonerLevel;
 		
 	}
 	
 	public int printPages(int pages) {
 		
+		pagesPrinted += pages;
 		
+		if(duplex){
+			System.out.println("This is a duplex printer.");
+			pagesPrinted /= (pagesPrinted/2);
+		}
+		
+		return pagesPrinted;
 		
 	}
 	
