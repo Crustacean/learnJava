ec1ecf51d261c9cd39f9e64c65a48113c98a358e
Elkana Muthengi <elkyem@gmail.com>
2023-11-17 22:06:43 +0300

    added Printer solution

---
+++ Printer/Printer.java
@@ -0,0 +1,19 @@
+public class Printer {
+	
+	private int tonerLevel;
+	private int pagesPrinted;
+	private boolean duplex;
+	
+	public int addToner(int tonerAmount) {
+		
+		
+		
+	}
+	
+	public int printPages(int pages) {
+		
+		
+		
+	}
+	
+}
