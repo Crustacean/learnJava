436c66a5feea7bc29a76bc26641bcb0ed9ef9d91
Elkana Muthengi <elkyem@gmail.com>
2023-11-17 20:37:21 +0300

    added Composition solution

--- Composition/Bed.java
+++ Composition/Bed.java
@@ -6,7 +6,7 @@ public class Bed {
 	private int sheets;
 	private int quilt;
 	
-	public Bed(String style, int pillows, int height, int sheets, int quilt;) {
+	public Bed(String style, int pillows, int height, int sheets, int quilt) {
 		
 		this.style = style;
 		this.pillows = pillows;
