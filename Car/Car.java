4a1e959822752c715732ec0e35073620437e1bb6
Elkana Muthengi <elkyem@gmail.com>
2023-11-18 19:39:30 +0300

    added Car solution

--- Car/Car.java
+++ Car/Car.java
@@ -2,21 +2,27 @@ public class Car {
 	
 	public String description;
 	
+	public Car(String description) {
+		
+		this.description = description;
+		
+	}
+	
 	public void startEngine() {
 		
-		
+		System.out.println("Start car engine.");
 		
 	}
 	
 	public void drive() {
 		
-		
+		System.out.println("Driving...");
 		
 	}
 	
 	protected void runEngine() {
 		
-		
+		System.out.println("Engine is running...");
 		
 	}
 	
--- Car/GasPoweredCar.java
+++ Car/GasPoweredCar.java
@@ -1,6 +1,13 @@
 public class GasPoweredCar {
 	
 	private double avgkmPerLitre;
-	private int cylinders;	
+	private int cylinders;
+	
+	public GasPoweredCar(double avgkmPerLitre, int cylinders) {
+		
+		this.avgkmPerLitre = avgkmPerLitre;
+		this.cylinders = cylinders;
+		
+	}
 	
 }
