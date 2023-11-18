0c05aaaa1807965186153ba3a21ff59b772663b1
Elkana Muthengi <elkyem@gmail.com>
2023-11-18 15:34:24 +0300

    added Encapsulation solution

--- Encapsulation/Printer.java
+++ Encapsulation/Printer.java
@@ -1,5 +1,18 @@
 public class Printer {
 	
+	private int tonerLevel;
+	private int pagesPrinted;
+	private boolean duplex;
 	
+	public Printer(int tonerLevel, boolean duplex) {
+		this(tonerLevel, duplex, 0);
+	}
+	
+	public Printer(int tonerLevel, boolean duplex, int pagesPrinted) {
+		
+		this.tonerLevel = (tonerLevel > -1 || tonerLevel <=100) ? tonerLevel : -1;
+		this.duplex = duplex;
+		this.pagesPrinted = pagesPrinted;
+	}
 	
 }
