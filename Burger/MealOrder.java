ced4ebefad1b25ea6b613753c138629064ae9ef1
Elkana Muthengi <elkyem@gmail.com>
2023-11-20 02:33:06 +0300

    added Burger solution

--- Burger/MealOrder.java
+++ Burger/MealOrder.java
@@ -47,8 +47,14 @@ public class MealOrder {
 		
 	}
 	
-	public void addBurgerToppings(String extra1, String extra2, String extra3 ) {
-		burger.addToppings(extra1, extra2, extra3);
+	public void addBurgerToppings(String extra1, String extra2, String extra3, String extra4, String extra5 ) {
+		
+		if(burger instanceOf DeluxeBurger db) {
+			db.addToppings(extra1, extra2, extra3, extra4, extra5);
+		}else {
+			burger.addToppings(extra1, extra2, extra3);
+		}
+		
 	}
 	
 	public void setDrinkSize(String size) {
