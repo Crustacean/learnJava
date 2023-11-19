6fb8b01c877cbf9d112067952576c23755679117
Elkana Muthengi <elkyem@gmail.com>
2023-11-20 02:28:10 +0300

    added Burger solution

--- Burger/MealOrder.java
+++ Burger/MealOrder.java
@@ -10,21 +10,38 @@ public class MealOrder {
 	
 	public MealOrder(String burgerType, String drinkType, String sideType) {
 		
-		this.burger = new Burger(burgerType, 4.0);
+		if(burgerType.equalsIgnoreCase("deluxe")) {
+			this.burger = new DeluxeBurger(burgerType, 8.5);
+		}else {
+			this.burger = new Burger(burgerType, 4.0);
+		}
+		
 		this.drink = new Item("drink", "Bamboocha", 0.6);
 		this.side = new Item("side", "mango juice", 0.4);
 		
 	}
 	
 	public double getTotalPrice() {
+		
+		if(burger instanceOf DeluxeBurger) {
+			return burger.getAdjustedPrice();
+		}
+		
 		return burger.getAdjustedPrice() + drink.getAdjustedPrice() + side.getAdjustedPrice();
 	}
 	
 	public void printItemizedList() {
 		
 		burger.printItem();
-		drink.printItem();
-		side.printItem();
+		
+		if(burger instanceOf DeluxeBurger) {
+			Item.printItem(drink.getName(), 0);
+			Item.printItem(side.getName(), 0);
+		}else {
+			drink.printItem();
+			side.printItem();
+		}
+		
 		System.out.println("-".repeat(50));
 		Item.printItem("TOTAL PRICE", getTotalPrice());
 		
