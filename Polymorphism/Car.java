b93a32d95d49e506cce495c0eb82ffc577a1d262
Elkana Muthengi <elkyem@gmail.com>
2023-11-19 03:32:48 +0300

    added Polymorphism solution

--- Polymorphism/Car.java
+++ Polymorphism/Car.java
@@ -1,5 +1,24 @@
 public class Car {
 	
+	private boolean engine;
+	private int cylinders;
+	private String name;
+	private int wheels;
+	
+	public Car(int cylinders, String name) {
+		this(true, cylinders, name, 4);
+	}
+	
+	public Car(boolean engine, int cylinders, String name, int wheels) {
+		this.engine = engine;
+		this.cylinders = cylinders;
+		this.name = name;
+		this.wheels = wheels;
+	}
+	
+	public void startEngine() {
+		System.out.println("Starting car...");
+	}
 	
 	
 }
