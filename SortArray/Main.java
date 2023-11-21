40371e7e7177d577fb97c07319058c905df508eb
Elkana Muthengi <elkyem@gmail.com>
2023-11-22 00:49:45 +0300

    added SortArray solution

--- SortArray/Main.java
+++ SortArray/Main.java
@@ -2,30 +2,29 @@ import java.util.*;
 
 public class Main {
 	
-	public static void main(string[] args) {
+	public static void main(String[] args) {
 		
 		int arraylength = 10;
 		
 		int[] arrayStorage = new int[arraylength];
 		arrayStorage = RandomArray.sortArray(arraylength);
 		
-		System.out.println(Arrays.toString(arrayStorage));
+		System.out.println("Randomly generated Array: " + Arrays.toString(arrayStorage));
 		
-		int[] copyArray = new int[arraylength];
-		
-		copyArray = Arrays.copyOf(arrayStorage);
+		int[] copyArray = Arrays.copyOf(arrayStorage, arraylength);
 		
 		Arrays.sort(arrayStorage);
 		
-		int[] descendingArray = new int[arraylength];
+		System.out.println("ArrayStorage sorted: " + Arrays.toString(arrayStorage));
 		
-		for(int i=arraylength; i>0; i--) {
-			for(int j=0; j<arraylength; j++) {
-				descendingArray[j] =  arrayStorage[i];
-			}
+		int[] descendingOrderArray = new int[arraylength];
+		
+		for(int i=0; i<arraylength; i++) {
+			//System.out.printf("%2d, ", arrayStorage[i]);
+			descendingOrderArray[i] = arrayStorage[arraylength-i-1];
 		}
 		
-		System.out.println(Arrays.toString(descendingArray));
+		System.out.println("Array sorted in descending order: " + Arrays.toString(descendingOrderArray));
 		
 	}
 	
