5933d99698de3065aae4d9752a69445c1491acd0
Elkana Muthengi <elkyem@gmail.com>
2023-11-21 23:54:44 +0300

    added Arr solution

--- Arr/Main.java
+++ Arr/Main.java
@@ -4,15 +4,25 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		//String stringArray = Arrays.toString(FillRandom.FillArray(10000));
-		//System.out.println(stringArray);
+		/*String stringArray = Arrays.toString(FillRandom.FillArray(10000));
+		System.out.println(stringArray);
 		
 		int[] arrayHolder = FillRandom.FillArray(10);
 		Arrays.sort(arrayHolder);
 		System.out.println(Arrays.toString(arrayHolder));
 		
 		int[] arrayCopy = Arrays.copyOf(arrayHolder, arrayHolder.length);
-		System.out.println(Arrays.toString(arrayCopy));
+		System.out.println(Arrays.toString(arrayCopy));*/
+		
+		String[] friendsList = {"June", "Kelvin", "Peter", "Mark", "John", "Violet", "Bancy"};		
+		Arrays.sort(friendsList);
+		if(Arrays.binarySearch(friendsList, "Bancy") > -1) {
+			System.out.println("Found!");
+		}else {
+			System.out.println("Not found.");
+		}
+		
+		
 		
 	}
 	
