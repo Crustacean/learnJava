8c5ba0ddb39a5954429f1d5b19849882155b88c3
Elkana Muthengi <elkyem@gmail.com>
2023-11-21 23:42:30 +0300

    added Arr solution

--- Arr/Main.java
+++ Arr/Main.java
@@ -4,9 +4,15 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		String stringArray = Arrays.toString(FillRandom.FillArray(10));
-		System.out.println(stringArray);
+		//String stringArray = Arrays.toString(FillRandom.FillArray(10000));
+		//System.out.println(stringArray);
 		
+		int[] arrayHolder = FillRandom.FillArray(10);
+		Arrays.sort(arrayHolder);
+		System.out.println(Arrays.toString(arrayHolder));
+		
+		int[] arrayCopy = Arrays.copyOf(arrayHolder, arrayHolder.length);
+		System.out.println(Arrays.toString(arrayCopy));
 		
 	}
 	
