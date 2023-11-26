98152154635fc3ec9ff9b6559e4ea7079b6f3054
Elkana Muthengi <elkyem@gmail.com>
2023-11-26 04:12:02 +0300

    added minimum Element solution

--- MinimumElement/Main.java
+++ MinimumElement/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		int arrays = MinimumElement.readIntegers();
+		int[] arrays = MinimumElement.readIntegers(5);
 		
 		System.out.println(Arrays.toString(arrays));
 		
