29fcef0b8e45941252ad223c16c7df488c2df6de
Elkana Muthengi <elkyem@gmail.com>
2023-11-26 03:57:50 +0300

    added minimum Element solution

--- MinimumElement/MinimumElement.java
+++ MinimumElement/MinimumElement.java
@@ -16,7 +16,19 @@ public class MinimumElement {
 		
 	}
 	
-	
+	public static int findMin(int[] arr) {
+		
+		int min = 0;
+		
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]<min) {
+				min = arr[i];
+			}
+		}
+		
+		return min;
+		
+	}
 	
 	
 }
