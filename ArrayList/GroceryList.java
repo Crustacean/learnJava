7a6f7ac2b58d67394ac0783e1ae6bb2abab9af73
Elkana Muthengi <elkyem@gmail.com>
2023-11-29 05:31:26 +0300

    added ArrayList solution

--- ArrayList/GroceryList.java
+++ ArrayList/GroceryList.java
@@ -1,24 +1,47 @@
-impor java.util.*;
+import java.util.*;
 
 public class GroceryList {
 	
-	public static void shutdown(int zero) {
-		System.out.println("Exiting application.");		
+	public static void shutdown() {
+		System.out.println("Exiting application.");	
+		System.exit(0);
 	}
 	
-	public static List<T> items(int one) {
+	public static void addItems(ArrayList<String> groceries) {
 		
-		Scanner scanner = new Scanner(System.in);s
+		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("Enter list of items separated by commas. Press 'Return/Enter' to exit." );
 		
 		String input = scanner.nextLine();
 		
-		String[] items = input.split(",").trim();
+		String[] items = input.split(",");
 		
-		List<String> list = List.of(items);
+		for(int i=0; i<items.length; i++) {
+			if(!groceries.contains(items[i].trim())) {
+				groceries.add(items[i].trim());
+			}
+		}
 		
-		return list;
+		System.out.println(groceries);
+		
+	}
+	
+	public static void removeItems(ArrayList<String> groceries) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter list of items separated by commas. Press 'Return/Enter' to exit." );
+		
+		String input = scanner.nextLine();
+		
+		String[] items = input.split(",");
+		
+		for(int i=0; i<items.length; i++) {
+			groceries.remove(items[i].trim());
+		}
+		
+		System.out.println(groceries);
 		
 	}
 	
--- ArrayList/Main.java
+++ ArrayList/Main.java
@@ -2,14 +2,34 @@ import java.util.*;
 
 public class Main {
 	
-	public static void main() {
+	private static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> groceries = new ArrayList<>();
 		
-		System.out.println("Enter an action: ");
+		boolean flag = true;
 		
+		while(flag) {
+			options();
+			switch(Integer.parseInt(scanner.nextLine())) {
+				case 0 -> GroceryList.shutdown();
+				case 1 -> GroceryList.addItems(groceries);
+				case 2 -> GroceryList.removeItems(groceries);
+				default -> flag = false;
+			}
+		}
 		
-		
+	}
+	
+	private static void options() {
+		String textBlock = """
+				Available Actions:
+				
+				0 - to Shutdown
+				1 - to Add items
+				2 - to Remove items """;
+		System.out.println(textBlock);
 	}
 	
 }
