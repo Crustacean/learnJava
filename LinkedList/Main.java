532b902d95e84c5f6597d471567480c7538aefe4
Elkana Muthengi <elkyem@gmail.com>
2023-12-04 01:15:59 +0300

    added Linked list solution

---
+++ LinkedList/Main.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+record Place(String name, int distance) {
+	
+	public Place {
+		Objects.requireNonNull(name);
+		Objects.requireNonNull(distance);
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s %d", name, distance);
+	}
+	
+}
+
+public class Main {
+	
+	public static void main(String... args) {
+		
+		LinkedList<Place> placesToVisit = new LinkedList<>();
+		
+		Place newPlace = new Place("Tokyo", 300);
+		addPlace(placesToVisit, newPlace);
+		addPlace(placesToVisit, new Place("Nairobi", 43000));
+		addPlace(placesToVisit, new Place("Arusha", 50000));
+		addPlace(placesToVisit, new Place("Zambia", 23000));
+		addPlace(placesToVisit, new Place("Bisbane", 90000));
+		addPlace(placesToVisit, new Place("local", 410));
+		placesToVisit.addFirst(new Place("Sydney", 0));
+		System.out.println(placesToVisit);
+		
+	}
+	
+	private static void addPlace(LinkedList<Place> listOfPlaces, Place place) {
+		
+		if(listOfPlaces.contains(place)) {
+			System.out.println("Found a duplicate. Not added "+place.name());
+			return;
+		}
+		
+		for(Place p : listOfPlaces) {
+			if(p.name().equalsIgnoreCase(place.name())) {
+				System.out.println("Found a duplicate. Not added "+place.name());
+				return;
+			}
+		}
+		
+		int matchedIndex = 0;
+		for(var listPlace : listOfPlaces) {
+			if(place.distance() < listPlace.distance()){
+				listOfPlaces.add(matchedIndex, place);
+				return;
+			}
+			matchedIndex++;
+		}
+		
+		listOfPlaces.add(place);
+		
+	}
+	
+	
+	
+}
