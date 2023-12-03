214c53895076fd96c14a908e30a34459ba0aa10c
Elkana Muthengi <elkyem@gmail.com>
2023-12-03 14:40:48 +0300

    added Mobile Phone solution

---
+++ MobilePhone/MobilePhone.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MobilePhone {
+	
+	private String myNumber;
+	ArrayList<Contact> myContacts = new ArrayList<>();
+	
+	public MobilePhone(String myNumber){
+		this.myNumber = myNumber;
+	}
+	
+}
