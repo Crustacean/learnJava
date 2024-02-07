eba47261392676575272cd4bcc411dc6b2d94cb0
Crustacean <elkyem@gmail.com>
2024-02-07 20:32:21 +0300

    Added Animal Solution

---
+++ Animal/Dog.java
@@ -0,0 +1,23 @@
+public class Dog extends Animal{
+	
+	public Dog(String type, String size, double weight) {
+		super(type, size, weight);
+	}
+	
+	@Override
+	public void move(String speed) {
+		
+	}
+	
+	@Override
+	public void makeNoise() {
+		
+		if (type == "Wolf") {
+			System.out.println("Howling!");			
+		}else{
+			System.out.println("Woof!");	
+		}
+		
+	}
+	
+}
