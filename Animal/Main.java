e2a7e3ea4a91822beb419629262d4d015d72e047
Crustacean <elkyem@gmail.com>
2024-02-07 21:07:36 +0300

    Added Animal Solution

--- Animal/Main.java
+++ Animal/Main.java
@@ -2,8 +2,17 @@ public class Main{
 	
 	public static void main(String[] args) {
 		
-		Animal animal = new Animal("animal", "big", 100);
+		Dog dog = new Dog("Wolf", "big", 100);
+		dog.makeNoise();
+		
+		doAnimalStuff(dog);
+		
+	}
+	
+	private static void doAnimalStuff(Animal animal) {
+		
 		animal.makeNoise();
+		animal.move("slow");
 		
 	}
 	
