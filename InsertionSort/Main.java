1ac6b4a5f3e30ffa7ca82400bc0c58824d984bd4
Crustacean <elkyem@gmail.com>
2024-03-19 20:48:10 +0300

    Added InsertionSort solution

---
+++ InsertionSort/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int[] intArray = {20, 35, 7, 89, 12, -1, 0, 89};
+		
+		for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+			
+			
+			
+		}
+		
+		printArray(intArray);
+		
+	}
+	
+	public static void printArray(int[] array) {
+		
+		for(int i=0; i<array.length; i++) {
+			System.out.println(array[i]);
+		}
+		
+	}	
+	
+}
