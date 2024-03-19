69ee071c2e9ef0548dd34b410ce3e6b1d53d1cca
Crustacean <elkyem@gmail.com>
2024-03-19 20:54:27 +0300

    Added InsertionSort solution

--- InsertionSort/Main.java
+++ InsertionSort/Main.java
@@ -6,6 +6,13 @@ public class Main {
 		
 		for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
 			
+			int newElement = intArray[firstUnsortedIndex];
+			
+			int i;
+			
+			for (i = firstUnsortedIndex; i>0 && intArray[i-1]>newElement; i--) {
+				intArray[i] = intArray[i-1];
+			}
 			
 			
 		}
