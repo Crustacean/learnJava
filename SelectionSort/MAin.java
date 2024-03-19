aca0aa0ee362175dcefc4ff875e8678bd64e4ff2
Crustacean <elkyem@gmail.com>
2024-03-19 20:07:18 +0300

    Added SelectionSort solution

---
+++ SelectionSort/MAin.java
@@ -0,0 +1,35 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int[] intArray = {20, 35, 7, 89, 12, -1, 0, 89};
+		
+		for(int lastUnsortedIndex = intArray.length-1; lastUnsortedIndex>0; lastUnsortedIndex--) {
+			
+			
+			
+		}
+		
+	}
+	
+	public static void swap(int[] array, int i, int j) {
+		
+		if (i == j) {
+			return;
+		}
+		
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+		
+	}
+	
+	public static void printArray(int[] array) {
+		
+		for(int i=0; i<array.length; i++) {
+			System.out.println(array[i]);
+		}
+		
+	}	
+	
+} 
